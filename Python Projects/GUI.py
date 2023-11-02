4904bf1eb5a64f624309c7b2a3188d2a6e3c9b65
rileydrewriley <rileydrewriley@gmail.com>
2023-11-02 15:57:29 -0700

    implemented the Add method

--- Python Projects/GUI.py
+++ Python Projects/GUI.py
@@ -4,21 +4,39 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todoItems', enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+
 
 window = sg.Window('To-Do App',
-                   layout=[[label, input_box, add_button]],
+                   layout=[[label, input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 10))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todoItems'])
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
+            todos.append(new_todo.title())
             functions.write_todos(todos)
+            window['todoItems'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todoItems'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todoItems'].update(values=todos)
+
+        case 'todoItems':
+            window['todo'].update(value=values['todoItems'][0])
         case sg.WIN_CLOSED:
             break
 
